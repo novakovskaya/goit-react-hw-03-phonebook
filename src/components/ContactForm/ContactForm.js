b936e4c4e85a7ebbf072dce088e945a70926cbad
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2021-12-12 21:50:24 +0200

    Added components for phonebook

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,70 @@
+import { Component } from "react";
+import { nanoid } from "nanoid";
+import styles from "./ContactForm.module.scss";
+
+class ContactForm extends Component {
+  state = {
+    name: "",
+    number: "",
+  };
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+    this.resetInput();
+  };
+
+  resetInput = () => {
+    this.setState({ name: "", number: "" });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    const nameId = nanoid();
+
+    return (
+      <form className={styles.Form} onSubmit={this.handleSubmit}>
+        <label className={styles.Label}>
+          Name
+          <input
+            className={styles.Input}
+            id={nameId}
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleInput}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label className={styles.Label}>
+          Number
+          <input
+            className={styles.Input}
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleInput}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+
+        <button type="submit" className={styles.Button}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
